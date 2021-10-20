4dc77b3897d4b9ba39b70071900605e40fc2cf73
DaveR0sman <david.ruiz.system@gmail.com>
2021-10-20 16:45:26 -0500

    Initial commit

---
+++ src/com/generation/Main.java
@@ -0,0 +1,28 @@
+package com.generation;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(Math.PI);
+        double num1 = 13.75;
+        double num2 = 9;
+        System.out.println(Math.sqrt(num2));
+    }
+}
+	// write your code here
+            /*String mensaje = saludar("david ruiz");
+            System.out.println(mensaje);
+            int resultado = suma(5, 3);
+            System.out.println(resultado);
+            alerta("mensaje de prueba");
+    }
+    public static String saludar(String nombre) {
+        return "Hola, como estas " + nombre;
+
+    }
+    public static int suma(int numero1, int numero2){
+        return numero1 + numero2;
+    }
+    public static void alerta(String mensaje) {
+        System.out.println(mensaje);*/
